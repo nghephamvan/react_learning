6c53d2ed9eabe56f13390aad0690ae20663f306f
Nghe Pham <pvnghe@tma.com.vn>
2020-07-17 10:18:14 +0700

    Init project

---
+++ ios/Pods/Target Support Files/Pods-HelloProject-HelloProjectTests/Pods-HelloProject-HelloProjectTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HelloProject_HelloProjectTests : NSObject
+@end
+@implementation PodsDummy_Pods_HelloProject_HelloProjectTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-HelloProject/Pods-HelloProject-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HelloProject : NSObject
+@end
+@implementation PodsDummy_Pods_HelloProject
+@end
